288c1a8d4deb09fbc1d4205eb36d870fcc34a7d4
luckyfullstack2023 <luckyfullstack2023@gmail.com>
2023-06-30 09:11:41 +0530

    sir L-47 assignment done

---
+++ js/script.js
@@ -0,0 +1,10 @@
+
+
+// function myFunction() {
+//     document.querySelector(".ooo").style.boxShadow = "2px 2px 3px red" ;
+//   }
+
+
+// let x = document.querySelector(".ooo")button:hover{
+//     boxShadow:2px 2px 2px red;
+// }
