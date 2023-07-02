d129bed4c9614241c98bef2ae93cfd0a29836db4
luckyfullstack2023 <luckyfullstack2023@gmail.com>
2023-07-02 13:09:38 +0530

    sir L-48 assignment done

--- js/script.js
+++ js/script.js
@@ -1,10 +1,49 @@
 
 
-// function myFunction() {
-//     document.querySelector(".ooo").style.boxShadow = "2px 2px 3px red" ;
-//   }
+let saveRegistrationInfo = () =>{
+    console.log("Hii Lucky you pass");
+            // object.method()
+    let fn = document.getElementById("first_name").value;
+    let ln = document.getElementById("last_name").value;
+
+    console.log(`My First Name is ${fn} and My Last Name is ${ln}`);
+
+    window.localStorage.setItem('first_name',fn);
+    window.localStorage.setItem('last_name',ln);
+
+       // page reload
+       window.location.reload();
+}
+
+let logout = () =>{
+    window.localStorage.clear();
+
+    // page reload
+    window.location.reload();
+}
+
+// ()(); IIFE
+(()=>{
+    let fn = window.localStorage.getItem('first_name');
+    let ln = window.localStorage.getItem('last_name');
 
 
-// let x = document.querySelector(".ooo")button:hover{
-//     boxShadow:2px 2px 2px red;
-// }
+    console.log('Page loaded successfully');
+
+    var modal = document.getElementById('regModel');
+
+    var modalInstance = new bootstrap.Modal(modal);
+
+    
+    
+    console.log(fn);
+    if(fn === null ){
+        // True
+        modalInstance.show();
+    }
+
+    if(fn !== null){
+        document.querySelector('.l_welcome').innerHTML='Welcome'+'  '+ fn+` `+ln+'<button class="btn btn-sm btn-danger ms-4 mt-2" onclick="logout()">Logout</button>'+'<button class="btn btn-sm btn-primary ms-4 mt-2" onclick="login()">Login</button>';
+    }
+
+})();
